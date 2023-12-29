06a12f154189e45f5107420729e0d811dc762a86
Aashif <uraashif@protonmail.com>
2023-12-29 14:51:39 +0530

    fix post viewing

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views as posts_views
-
+import uuid
 
 urlpatterns = [
     path('create/', view=posts_views.create_post, name='create_post'),
-    path('<int:post_id>', view=posts_views.view_post, name='view_post'),
+    path('<uuid:post_id>', view=posts_views.view_post, name='view_post'),
     path('like', view=posts_views.like_post, name='like'),
     path('comment', view=posts_views.comment_post, name='comment'),
 ]
